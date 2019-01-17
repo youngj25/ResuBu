af956715855629a0e5e52adfbea4178504ff8d3d
Jason A. Young <youngj25@southernct.edu>
2019-01-16 20:07:45 -0500

    Starting Development

---
+++ src/code/Data_Importer.java
@@ -0,0 +1,16 @@
+package code;
+
+/**
+ * Data Importer
+ * @author Jason A. Young
+ *
+ * The purpose of this class is to handle all of the
+ * file imports and managements. 
+ */
+public class Data_Importer {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/code/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Jason
+ *
+ */
+package code;
